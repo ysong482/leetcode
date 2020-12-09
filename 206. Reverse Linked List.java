b3053149466bed53c22bcae42ed20e74aaf5858f
ktfairy <60088694+ktfairy@users.noreply.github.com>
2020-12-09 08:49:36 -0800

    Update 206. Reverse Linked List.java

--- 206. Reverse Linked List.java
+++ 206. Reverse Linked List.java
@@ -6,6 +6,7 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+//中规中矩用迭代，
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode dummy = null;
@@ -16,5 +17,25 @@ class Solution {
             dummy = temp;
         }
         return dummy;
+    }
+}
+
+//使用栈来实现倒序
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        Stack<Integer> st = new Stack<Integer>();
+        if(head == null) return null;
+        while(head.next != null){
+            st.push(head.val);
+            head = head.next;
+        }
+        ListNode dmuuy = head;
+
+        while(!st.isEmpty()){
+            ListNode node = new ListNode(st.pop());
+            dmuuy.next = node;
+            dmuuy = dmuuy.next;
+        }
+        return head;
     }
 }
