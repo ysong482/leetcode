ecfb9747a05eff6ea3b1b5c7c66cefce84311e4a
ktfairy <60088694+ktfairy@users.noreply.github.com>
2020-12-26 07:24:33 -0800

    Update 441. Arranging Coins.py

--- 441. Arranging Coins.py
+++ 441. Arranging Coins.py
@@ -5,3 +5,12 @@ class Solution:
             n = n-line
             line = line +1
         return line-1
+#傻办法
+    
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        line = 1
+        while (line*(1+line)/2 <= n):
+            line = line +1
+        return line-1
+#利用等差数列求和公式
